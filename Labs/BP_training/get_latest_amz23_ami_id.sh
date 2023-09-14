164b0e737ce41d41cb9cc7bd1e651594b7825577
MarcinDudek <dudek.v@gmail.com>
2023-09-14 11:32:02 +0000

    Add data "external" bash script

--- Labs/BP_training/get_latest_amz23_ami_id.sh
+++ Labs/BP_training/get_latest_amz23_ami_id.sh
@@ -3,4 +3,4 @@
 AMI_NAME="/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64"
 
 AMI_ID=$(aws --no-cli-auto-prompt ssm get-parameter --name $AMI_NAME --query "Parameter.Value" --output text)
-echo "\"$AMI_ID\""
+echo "{\"ami_id\": \"$AMI_ID\"}"
--- Labs/BP_training/main.tf
+++ Labs/BP_training/main.tf
@@ -11,7 +11,18 @@ resource "aws_instance" "amz_linux" {
   }
 }
 
+# --- Task 2 ---
 output "server_ip" {
   description = "Public IP of created server"
   value       = aws_instance.amz_linux.public_ip
+}
+
+# --- Task 3 ---
+data "external" "ami_id" {
+  program = ["bash", "${path.module}/get_latest_amz23_ami_id.sh"]
+}
+
+output "ami_id" {
+  description = "Latest AMZ 2023 Linux"
+  value       = data.external.ami_id.result["ami_id"]
 }
