0d06ed96be7f0b282e6bab514875534a5277f4e1
MarcinDudek <dudek.v@gmail.com>
2023-09-15 09:00:17 +0000

    Add Task 4,5

--- Labs/BP_training/main.tf
+++ Labs/BP_training/main.tf
@@ -1,5 +1,10 @@
 provider "aws" {
   region = "eu-north-1"
+  default_tags {              # --- Task 4 ---
+    tags = {
+      tf_purpose = "training"
+    }
+  }
 }
 
 resource "aws_instance" "amz_linux" {
@@ -22,7 +27,36 @@ data "external" "ami_id" {
   program = ["bash", "${path.module}/get_latest_amz23_ami_id.sh"]
 }
 
-output "ami_id" {
-  description = "Latest AMZ 2023 Linux"
+output "ami_id_from_script" {
+  description = "Latest AMZ 2023 Linux from script"
   value       = data.external.ami_id.result["ami_id"]
+}
+
+# --- Task 5 ---
+data "aws_ami" "latest_ubunut" {
+  most_recent = true
+  name_regex = "^ubuntu/images/hvm-ssd/ubuntu*"
+  # name_regex = "^amazon/ubuntu/images/hvm-ssd/ubuntu*"
+  owners = ["099720109477"]
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+
+  # --- minimort ---
+  # filter {
+  #   name = "root-device-type"
+  #   values = ["ebs"]
+  # }
+}
+
+output "ami_id_from_data_source" {
+  description = "Latest Ubuntu Linux from Data Source"
+  value       = data.aws_ami.latest_ubunut.id
 }
